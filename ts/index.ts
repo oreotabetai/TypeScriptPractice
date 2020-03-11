768924508e584e0c594dd9291db4b4adef0afb26
oreotabetai <s17t213@stu.kagawa-u.ac.jp>
2020-03-11 14:20:02 +0900

    add User.ts

--- ts/index.ts
+++ ts/index.ts
@@ -1,13 +1,4 @@
-class User {
-    public age: number;
-    public familyName: string;
-    public givenName: string;
-    constructor(familyName: string, givenName: string, age: number) {
-        this.age = age;
-        this.familyName = familyName;
-        this.givenName = givenName;
-    }
-}
+import { User } from "./User";
 
 const user = new User('鈴木', '太郎', 44);
 
