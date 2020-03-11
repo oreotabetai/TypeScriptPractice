da3de1a23d9afb6f3bab47a9b887c4cc695144ba
oreotabetai <s17t213@stu.kagawa-u.ac.jp>
2020-03-11 15:22:14 +0900

    get uuid

--- ts/index.ts
+++ ts/index.ts
@@ -1,3 +1,5 @@
+import { uuid } from 'uuidv4';
+
 import { User } from "./User";
 
 const user = new User('鈴木', '太郎', 44);
@@ -5,4 +7,11 @@ const user = new User('鈴木', '太郎', 44);
 const contentsElem = document.getElementById('contents');
 if (!!contentsElem) {
     contentsElem.innerText = `${user.familyName} ${user.givenName}`;
+}
+
+const saibanButton = document.getElementById('saiban') as HTMLButtonElement;
+const uuidSpan = document.getElementById('uuid') as HTMLSpanElement;
+
+saibanButton.onclick = (e) => {
+    uuidSpan.innerText = uuid();
 }
